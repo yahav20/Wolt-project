923ee5a328ad9b383244f84eabfea46d90a8c9e6
idorozin <rozin.ido@gmail.com>
2024-12-31 21:59:43 +0200

    add docker

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const signout = require('./routes/signout')
 const session = require('express-session');
 console.log(process.env.NODE_ENV);
 require('custom-env').env(process.env.NODE_ENV,'./config');
-mongoose.connect(process.env.CONNECTION_STRING)
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mydatabase';
 
+mongoose.connect(process.env.MONGO_URI)
 var app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -23,4 +24,4 @@ app.use('/login', login);
 app.use('/dashboard', dashboard)
 app.use('/signup', signup)
 app.use('/signout', signout)
-app.listen(process.env.PORT);
+app.listen(3000);
