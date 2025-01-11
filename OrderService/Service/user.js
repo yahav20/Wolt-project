91932eb80a51c37725070382d9cad3f4ff817b5d
yahav20 <yahavezer256@gmail.com>
2025-01-11 22:37:51 +0200

    rename getRestaurantById to getUserById and add updateOrderHistory function

--- OrderService/Service/user.js
+++ OrderService/Service/user.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const USER_SERVICE = "http://localhost:5000/restaurants";
 
-async function getRestaurantById(id) {
+async function getUserById(id) {
   try {
     const user = await axios.get(`${USER_SERVICE}/${id}`);
     return user;
@@ -11,4 +11,12 @@ async function getRestaurantById(id) {
   }
 }
 
-module.exports = { getRestaurantById };
+async function updateOrderHistory(id, order) {
+  try {
+    await axios.post(`${USER_SERVICE}/${id}/orders`, order);
+  } catch (error) {
+    throw new Error("Failed to update user order history");
+  }
+}
+
+module.exports = { getUserById , updateOrderHistory };
