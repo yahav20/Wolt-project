63978208945ec2ed9a583f44142acb6ea9e0799f
yahav20 <yahavezer256@gmail.com>
2025-01-12 14:08:21 +0200

    update .gitignore and change USER_SERVICE endpoint to users

--- OrderService/Service/user.js
+++ OrderService/Service/user.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const USER_SERVICE = "http://localhost:5000/restaurants";
+const USER_SERVICE = "http://localhost:5000/users";
 
 async function getUserById(id) {
   try {
