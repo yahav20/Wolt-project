204c54892267b8e1f9a32f310a050a99f7133794
idorozin <rozin.ido@gmail.com>
2025-01-14 19:17:56 +0200

    update

--- Router/routes/api/users.js
+++ Router/routes/api/users.js
@@ -20,5 +20,17 @@ router.use(
     changeOrigin: true,
   })
 );
+router.use(
+  '/id/:id',
+  createProxyMiddleware({
+    target: USERS_SERVICE,
+    changeOrigin: true,
+    pathRewrite: (path, req) => {
+      // Rewrite the path to include the :id parameter in the target URL
+      const userId = req.params.id;
+      return `/users/${userId}`;
+    },
+  })
+);
 
 module.exports = router;
